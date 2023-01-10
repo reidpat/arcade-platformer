776c3347019b1dd9e542aab43f88753eda302630
AlexG-A01234719 <71039819+AlexG-A01234719@users.noreply.github.com>
2023-01-10 11:04:13 -0800

    New stickman character sprite

--- main.ts
+++ main.ts
@@ -71,24 +71,7 @@ controller.left.onEvent(ControllerButtonEvent.Released, function () {
 let mySprite: Sprite = null
 tiles.setTilemap(tilemap`level1`)
 scene.setBackgroundColor(2)
-mySprite = sprites.create(img`
-    . . . . . . f f f f f f . . . . 
-    . . . . f f e e e e f 2 f . . . 
-    . . . f f e e e e f 2 2 2 f . . 
-    . . . f e e e f f e e e e f . . 
-    . . . f f f f e e 2 2 2 2 e f . 
-    . . . f e 2 2 2 f f f f e 2 f . 
-    . . f f f f f f f e e e f f f . 
-    . . f f e 4 4 e b f 4 4 e e f . 
-    . . f e e 4 d 4 1 f d d e f . . 
-    . . . f e e e 4 d d d d f . . . 
-    . . . . f f e e 4 4 4 e f . . . 
-    . . . . . 4 d d e 2 2 2 f . . . 
-    . . . . . e d d e 2 2 2 f . . . 
-    . . . . . f e e f 4 5 5 f . . . 
-    . . . . . . f f f f f f . . . . 
-    . . . . . . . f f f . . . . . . 
-    `, SpriteKind.Player)
+mySprite = sprites.create(assets.image`myImage`, SpriteKind.Player)
 mySprite.ay = 200
 tiles.placeOnTile(mySprite, tiles.getTileLocation(2, 12))
 scene.cameraFollowSprite(mySprite)
